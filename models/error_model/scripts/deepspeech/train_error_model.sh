8257aec793a7d5ddcbdf518cdfb91b01ee116a72
jiekeshi <jiekeshi125@gmail.com>
2022-01-14 16:56:28 +0800

    scripts: training error model

--- models/error_model/scripts/deepspeech/train_error_model.sh
+++ models/error_model/scripts/deepspeech/train_error_model.sh
@@ -6,24 +6,24 @@ for seed in 1
 do
   for accent in "${accents[@]}"
   do
-    LR=3e-4
+    LR=1e-4
     echo $accent seed $seed
-    python3 -u train_error_model.py \
-      --batch_size=100 \
+    CUDA_VISIBLE_DEVICES=1 python3 -u train_error_model.py \
+      --batch_size=32 \
       --num_epochs=200 \
       --train_freq=20 \
       --lr=$LR \
-      --num_layers=4 \
-      --hidden_size=64 \
-      --input_size=64 \
+      --num_layers=2 \
+      --hidden_size=128 \
+      --input_size=128 \
       --weight_decay=0.001 \
       --train_portion=0.8 \
       --hypotheses_path=$DATA/$accent/manifests/deepspeech_outputs/seed_plus_dev_out.txt \
       --lr_decay=warmup \
       --seed=$seed \
       --output_dir=$PRETRAINED_CKPTS/error_models/deepspeech/$accent/seed_"$seed"/recent \
-      --best_dir=$PRETRAINED_CKPTS/error_models/deepspeech/$accent/seed_"$seed"/best \
-      --pretrained_ckpt=$PRETRAINED_CKPTS/error_models/librispeech/seed_"$seed"/best/ErrorClassifierPhoneBiLSTM_V2.pt
+      --best_dir=$PRETRAINED_CKPTS/error_models/deepspeech/$accent/seed_"$seed"/best 
+      # --pretrained_ckpt=$PRETRAINED_CKPTS/error_models/librispeech/seed_"$seed"/best/ErrorClassifierPhoneBiLSTM_V2.pt
   echo
   done
 done
