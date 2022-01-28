be59055b102c248fc098cf15004a5f285eeac877
yangzhou6666 <zhou.yang.19@ucl.ac.uk>
2022-01-28 16:09:02 +0800

    feat(l2arctic): analyze the results

--- data/l2arctic/analyze.py
+++ data/l2arctic/analyze.py
@@ -7,10 +7,12 @@ path = './processed'
 
 for name in os.listdir(path):
     # Get original performance on seed_plus_dev.json
-    log_path = os.path.join(path, name, 'manifests', 'quartznet_outputs', 'seed_plus_dev_infer_log.txt')
+    
     print('>>>>>>>>>>>>>>>>')
     print(name)
 
+    print("------Original Performance------")
+    log_path = os.path.join(path, name, 'manifests', 'quartznet_outputs', 'seed_plus_dev_infer_log.txt')
     with open(log_path) as f:
         contents = f.readlines()
         WER = contents[-6].strip()
@@ -18,3 +20,31 @@ for name in os.listdir(path):
         CER = contents[-2].strip()
         print(WER)
         print(CER)
+    
+    print("------Randome Selection------")
+    baseline_log = f'../../models/pretrained_checkpoints/quartznet/finetuned/{name}/200/seed_1/random/test_infer_log.txt'
+    if os.path.exists(baseline_log): 
+        with open(baseline_log) as f:
+            contents = f.readlines()
+            WER = contents[-6].strip()
+
+            CER = contents[-2].strip()
+            print(WER)
+            print(CER)
+    else:
+        print("\033[0;37;41m\t!!Data Missing!!\033[0m")
+
+    print("------ICASSP 2021------")
+    baseline_log = f'../../models/pretrained_checkpoints/quartznet/finetuned/{name}/200/seed_1/error_model/test_infer_log.txt'
+    if os.path.exists(baseline_log): 
+        with open(baseline_log) as f:
+            contents = f.readlines()
+            WER = contents[-6].strip()
+
+            CER = contents[-2].strip()
+            print(WER)
+            print(CER)
+    else:
+        print("\033[0;37;41m\t!!Data Missing!!\033[0m")
+
+    print("\n\n")
