d15b861ed253d1ad863938f244a79193f0f6a8b2
yangzhou6666 <zhou.yang.19@ucl.ac.uk>
2022-02-03 17:41:55 +0800

    feat(): infer on synthetic data

--- models/quartznet_asr/scripts/l2arctic/infer_transcriptions_on_seed_set.sh
+++ models/quartznet_asr/scripts/l2arctic/infer_transcriptions_on_seed_set.sh
@@ -31,3 +31,35 @@ do
   --ckpt=$PRETRAINED_CKPTS/quartznet/librispeech/quartznet.pt \
   > $DATA/$accent/manifests/quartznet_outputs/seed_plus_dev_infer_log_tts.txt &
 done
+
+
+for accent in "${accents[@]}"
+do
+  mkdir -p $DATA/$accent/manifests/quartznet_outputs
+  echo $accent
+  echo $WAV_DIR/$accent/wav 
+  CUDA_VISIBLE_DEVICES=0 python3 -u inference.py \
+  --batch_size=16 \
+  --output_file=$DATA/$accent/manifests/quartznet_outputs/selection_tts.txt \
+  --wav_dir=$WAV_DIR \
+  --val_manifest=$DATA/$accent/manifests/selection_tts.json \
+  --model_toml=$PRETRAINED_CKPTS/quartznet/quartznet15x5.toml \
+  --ckpt=$PRETRAINED_CKPTS/quartznet/librispeech/quartznet.pt \
+  > $DATA/$accent/manifests/quartznet_outputs/selection_log_tts.txt &
+done
+
+
+for accent in "${accents[@]}"
+do
+  mkdir -p $DATA/$accent/manifests/quartznet_outputs
+  echo $accent
+  echo $WAV_DIR/$accent/wav 
+  CUDA_VISIBLE_DEVICES=0 python3 -u inference.py \
+  --batch_size=16 \
+  --output_file=$DATA/$accent/manifests/quartznet_outputs/selection.txt \
+  --wav_dir=$WAV_DIR \
+  --val_manifest=$DATA/$accent/manifests/selection_tts.json \
+  --model_toml=$PRETRAINED_CKPTS/quartznet/quartznet15x5.toml \
+  --ckpt=$PRETRAINED_CKPTS/quartznet/librispeech/quartznet.pt \
+  > $DATA/$accent/manifests/quartznet_outputs/selection_log.txt &
+done
