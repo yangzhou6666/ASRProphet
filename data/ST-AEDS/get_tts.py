6c3022bf8c2463c436604cf9425cd928f90cb77a
Jieke SHI <jiekeshi125@gmail.com>
2022-02-03 15:18:31 +0800

    chore: ignore data

--- data/ST-AEDS/get_tts.py
+++ data/ST-AEDS/get_tts.py
@@ -1,15 +1,15 @@
 import os
 import json
-from gtts import gTTS
+# from gtts import gTTS
 from tqdm import tqdm
 
-def googleGenerateAudio(text, audio_path):
-    tempfile = audio_path + "_temp.mp3"
-    googleTTS = gTTS(text, lang='en-us')
-    googleTTS.save(tempfile)
-    setting = " -acodec pcm_s16le -ac 1 -ar 16000 "
-    os.system(f"ffmpeg -loglevel error -i {tempfile} {setting} {audio_path} -y")
-    os.remove(tempfile)
+# def googleGenerateAudio(text, audio_path):
+#     tempfile = audio_path + "_temp.mp3"
+#     googleTTS = gTTS(text, lang='en-us')
+#     googleTTS.save(tempfile)
+#     setting = " -acodec pcm_s16le -ac 1 -ar 16000 "
+#     os.system(f"ffmpeg -loglevel error -i {tempfile} {setting} {audio_path} -y")
+#     os.remove(tempfile)
 
 def rvGenerateAudio(text, audio_path):
     tempfile = audio_path + "_temp.mp3"
@@ -20,25 +20,25 @@ def rvGenerateAudio(text, audio_path):
     os.remove(tempfile)
     
 def macGenerateAudio(text, audio_path):
-    tempfile = audio_path + "_temp.mp3"
-    cmd = "say -v Alex -o {tempfile} \"{text}\""
+
+    cmd = "say -v Alex -o "+ audio_path + " \""+ text + "\""
     os.system(cmd)
-    setting = " -acodec pcm_s16le -ac 1 -ar 16000 "
-    os.system(f"ffmpeg -loglevel error -i {tempfile} {setting} {audio_path} -y")
-    os.remove(tempfile)
+    # setting = " -acodec pcm_s16le -ac 1 -ar 16000 "
+    # os.system(f"ffmpeg -loglevel error -i temp.aiff {setting} {audio_path} -y")
+    # os.remove("temp.aiff")
 
-os.makedirs("/workspace/ASRDebugger/data/LibriSpeech/TTS/")
+os.makedirs("./TTS", exist_ok=True)
 
-with open("./manifests/all.json") as f, open("./TTS/all.json", "a") as w:
+with open("./all.json") as f, open("./TTS/all.json", "w") as w:
     count = 0
     texts = []
     for line in f.readlines():
         item = json.loads(line)
-        texts.append((item["audio_filepath"], item["text"]))
+        texts.append((item["audio_filepath"].split("/")[-1].split(".")[0], item["text"]))
 
     for text in tqdm(texts):
-        macGenerateAudio(text[1], "/workspace/ASRDebugger/data/LibriSpeech/TTS/"+text[0].split("/")[-1])
-        json.dump({"read_audio": text[0], "text": text[1], "audio_filepath": "/workspace/ASRDebugger/data/LibriSpeech/TTS/"+str(index)+".wav"}, w)
+        macGenerateAudio(text[1], "./TTS/"+text[0])
+        json.dump({"text": text[1], "audio_filepath": "/workspace/ASRDebugger/data/LibriSpeech/TTS/"+text[0]+".aiff"}, w)
         w.write("\n")
 
 
