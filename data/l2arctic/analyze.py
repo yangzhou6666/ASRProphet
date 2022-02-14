786a437bd007416364244a0ffc82007bbcc7b26d
yangzhou6666 <zhou.yang.19@ucl.ac.uk>
2022-02-14 15:37:38 +0800

    refactor(l2arctic): make the code cleaner

--- data/l2arctic/analyze.py
+++ data/l2arctic/analyze.py
@@ -2,27 +2,7 @@ import os
 
 path = './processed'
 
-
-
-
-for name in os.listdir(path):
-    # Get original performance on seed_plus_dev.json
-    
-    print('>>>>>>>>>>>>>>>>')
-    print(name)
-
-    print("------Original Performance on Real audio------")
-    log_path = os.path.join(path, name, 'manifests', 'quartznet_outputs', 'seed_plus_dev_infer_log.txt')
-    with open(log_path) as f:
-        contents = f.readlines()
-        WER = contents[-6].strip()
-
-        CER = contents[-2].strip()
-        print(WER)
-        print(CER)
-
-    print("------Original Performance on TTS------")
-    log_path = os.path.join(path, name, 'manifests', 'quartznet_outputs', 'seed_plus_dev_infer_log_tts.txt')
+def print_result(log_path):
     if os.path.exists(log_path): 
         with open(log_path) as f:
             contents = f.readlines()
@@ -33,85 +13,45 @@ for name in os.listdir(path):
             print(CER)
     else:
         print("\033[0;37;41m\t!!Data Missing!!\033[0m")
+
+for name in os.listdir(path):
+    # Get original performance on seed_plus_dev.json
+    
+    print('>>>>>>>>>>>>>>>>')
+    print(name)
+
+    print("------Original Performance on Seed + Dev ------")
+    log_path = os.path.join(path, name, 'manifests', 'quartznet_outputs', 'seed_plus_dev_infer_log.txt')
+    print_result(log_path)
+
+    print("------Original Performance on Synthetic Seed + Dev------")
+    log_path = os.path.join(path, name, 'manifests', 'quartznet_outputs', 'seed_plus_dev_infer_log_tts.txt')
+    print_result(log_path)
     
     print("------Randome Selection------")
     baseline_log = f'../../models/pretrained_checkpoints/quartznet/finetuned/{name}/200/seed_1/random/test_infer_log.txt'
-    if os.path.exists(baseline_log): 
-        with open(baseline_log) as f:
-            contents = f.readlines()
-            WER = contents[-6].strip()
-
-            CER = contents[-2].strip()
-            print(WER)
-            print(CER)
-    else:
-        print("\033[0;37;41m\t!!Data Missing!!\033[0m")
+    print_result(log_path)
 
     print("------Randome TTS------")
     baseline_log = f'../../models/pretrained_checkpoints/quartznet/finetuned/{name}/200/seed_1/random_tts/test_infer_log.txt'
-    if os.path.exists(baseline_log): 
-        with open(baseline_log) as f:
-            contents = f.readlines()
-            WER = contents[-6].strip()
-
-            CER = contents[-2].strip()
-            print(WER)
-            print(CER)
-    else:
-        print("\033[0;37;41m\t!!Data Missing!!\033[0m")
+    print_result(log_path)
 
     print("------ICASSP 2021------")
     baseline_log = f'../../models/pretrained_checkpoints/quartznet/finetuned/{name}/200/seed_1/error_model/test_infer_log.txt'
-    if os.path.exists(baseline_log): 
-        with open(baseline_log) as f:
-            contents = f.readlines()
-            WER = contents[-6].strip()
-
-            CER = contents[-2].strip()
-            print(WER)
-            print(CER)
-    else:
-        print("\033[0;37;41m\t!!Data Missing!!\033[0m")
+    print_result(log_path)
 
     print("------Selected TTS------")
     baseline_log = f'../../models/pretrained_checkpoints/quartznet/finetuned/{name}/200/seed_1/error_model_tts/test_infer_log.txt'
-    if os.path.exists(baseline_log): 
-        with open(baseline_log) as f:
-            contents = f.readlines()
-            WER = contents[-6].strip()
-
-            CER = contents[-2].strip()
-            print(WER)
-            print(CER)
-    else:
-        print("\033[0;37;41m\t!!Data Missing!!\033[0m")
+    print_result(log_path)
 
 
     print("------Upper Bound------")
     baseline_log = f'../../models/pretrained_checkpoints/quartznet/finetuned/{name}/200/seed_1/all/test_infer_log.txt'
-    if os.path.exists(baseline_log): 
-        with open(baseline_log) as f:
-            contents = f.readlines()
-            WER = contents[-6].strip()
-
-            CER = contents[-2].strip()
-            print(WER)
-            print(CER)
-    else:
-        print("\033[0;37;41m\t!!Data Missing!!\033[0m")
+    print_result(log_path)
 
 
     print("------ All TTS------")
     baseline_log = f'../../models/pretrained_checkpoints/quartznet/finetuned/{name}/200/seed_1/all_tts/test_infer_log.txt'
-    if os.path.exists(baseline_log): 
-        with open(baseline_log) as f:
-            contents = f.readlines()
-            WER = contents[-6].strip()
-
-            CER = contents[-2].strip()
-            print(WER)
-            print(CER)
-    else:
-        print("\033[0;37;41m\t!!Data Missing!!\033[0m")
+    print_result(log_path)
 
     print("\n\n")
