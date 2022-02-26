9e16d0a05fa3b7a8c6cddf8143238b2899cf1de2
yangzhou6666 <zhou.yang.19@ucl.ac.uk>
2022-02-26 21:41:57 +0800

    feat(err_predictor): save the best model

--- models/error_model/word_error_predictor.py
+++ models/error_model/word_error_predictor.py
@@ -175,11 +175,13 @@ def main(args):
     training_args = TrainingArguments(
         f"word_error_predictor",
         evaluation_strategy = "epoch",
+        save_strategy = "epoch",
         learning_rate=2e-5,
         per_device_train_batch_size=16,
         per_device_eval_batch_size=16,
         num_train_epochs=20,
         weight_decay=0.01,
+        load_best_model_at_end=True
     )
 
     trainer = Trainer(
