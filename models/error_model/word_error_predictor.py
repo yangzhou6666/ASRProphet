5b02df5badfef05463ec72f122ba2a1c0a7d9fe8
yangzhou6666 <zhou.yang.19@ucl.ac.uk>
2022-02-26 14:49:11 +0800

    feat(get_label): deal with special characters

--- models/error_model/word_error_predictor.py
+++ models/error_model/word_error_predictor.py
@@ -4,8 +4,10 @@ sys.path.append("..")
 from quartznet_asr.metrics import __levenshtein, word_error_rate
 from power import Levenshtein, ExpandedAlignment
 from power.aligner import PowerAligner
-
-
+from transformers import AutoTokenizer
+from transformers import BertTokenizer, BertForTokenClassification
+import torch
+import re
 def get_label(path: str):
     '''
     Given the path to the reference and transcriptions
@@ -22,8 +24,9 @@ def get_label(path: str):
             hyp = data[4][5:]
 
             # convert string to list of words
-            ref = [x for x in ref.strip().split(' ') if x]
-            hyp = [x for x in hyp.strip().split(' ') if x]
+            re_expr = "\'| "
+            ref = [x for x in re.split(re_expr, ref.strip()) if x]
+            hyp = [x for x in re.split(re_expr, hyp.strip()) if x]
 
             lev = Levenshtein.align(ref, hyp) 
             lev.editops()
@@ -56,9 +59,8 @@ def get_label(path: str):
             labels.append(label)
             inputs.append(alignment.ref())
     
-    return labels
+    return inputs, labels
 
 if __name__ == "__main__":
     path_to_result = '/workspace/data/l2arctic/processed/ASI/manifests/train/quartznet/error_model_tts/500/seed_1/test_out_ori.txt'
-    get_label(path_to_result)
 
