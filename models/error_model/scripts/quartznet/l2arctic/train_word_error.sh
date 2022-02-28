d3f1a91664b896d2bdb7eae87da5930e4e511f14
jiekeshi <jiekeshi125@gmail.com>
2022-02-28 10:45:24 +0800

    doc: scripts to run sampling and testing

--- models/error_model/scripts/quartznet/l2arctic/train_word_error.sh
+++ models/error_model/scripts/quartznet/l2arctic/train_word_error.sh
@@ -10,7 +10,7 @@ do
   mkdir -p $PRETRAINED_CKPTS/word_error_predictor/quartznet/$accent/seed_"$seed"/best
   CUDA_VISIBLE_DEVICES=1 python word_error_predictor.py \
     --train_path=$DATA/$accent/manifests/quartznet_outputs/seed_out.txt \
-    --test_path=$DATA/$accent/manifests/quartznet_outputs/dev_out.txt \
+    --test_path=$DATA/$accent/manifests/quartznet_outputs/selection_out_tts.txt \
     --output_dir=$PRETRAINED_CKPTS/word_error_predictor/quartznet/$accent/seed_"$seed"/best \
     > $PRETRAINED_CKPTS/word_error_predictor/quartznet/$accent/seed_"$seed"/training.log
   done
