e59f546d87edcbed76087e94ee545f64bc9fe513
yangzhou6666 <zhou.yang.19@ucl.ac.uk>
2022-03-02 14:19:59 +0800

    feat(sample): phoneme-diversity enhancing

--- models/error_model/word_error_sampling.py
+++ models/error_model/word_error_sampling.py
@@ -13,8 +13,29 @@ import pandas as pd
 import numpy as np
 import argparse
 import random
+import string
 from tqdm import tqdm
 import torch.nn.functional as F
+from g2p_en import G2p
+
+labels = [" ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "'"]
+punctuation = string.punctuation
+punctuation = punctuation.replace("+", "")
+punctuation = punctuation.replace("&", "")
+for l in labels:
+    punctuation = punctuation.replace(l, "")
+table = str.maketrans(punctuation, " " * len(punctuation))
+
+
+MASK = '<mask>'
+SOS = '<s>'
+EOS = '</s>'
+
+
+get_phoneme_seq = G2p()
+phone_vocab = get_phoneme_seq.phonemes[0:4]
+phone_vocab += np.unique([item if len(item)!=3 else item[:-1] for item in get_phoneme_seq.phonemes[4:]]).tolist()
+phone_vocab += [MASK, ' ']
 
 
 def parse_args():
@@ -39,6 +60,8 @@ def parse_args():
     args=parser.parse_args()
     return args
 
+def get_f(freq,tau=500):
+    return 1 - np.exp(-freq/tau)
 
 def format_data(json_fpath):
     file = open(json_fpath)
@@ -102,6 +125,8 @@ class WordErrorSampler():
         # path to the selection json file (the json file that contains the test cases)
         self.selection_json_file = selection_json_file
 
+
+
     def load_model(self):
         '''Load tokenizer and model'''
 
@@ -142,6 +167,7 @@ class WordErrorSampler():
         '''select the test cases based on sum of their error score'''
         score = [sum(n)/len(n) for n in self.res]
 
+        # rank the test cases based on the score
         test_texts, fpaths, score, durations = sort(self.selection_texts, self.selection_fpaths, score, self.selection_durations)
         samples = []
         selected_duration = 0.0
@@ -156,12 +182,77 @@ class WordErrorSampler():
 
         return samples
 
-    def sample_with_phone_enhancing(self, duration: float):
-        pass
+    def sample_with_phone_enhancing(self, required_duration: float):
+        '''select the test cases based on sum of their error score and consider the phone diversisty enhancing'''
+
+        # we cannot pre-compute a static metric for all texts in advance
+        # we need to compute it on the fly
+
+        # rank the test cases based on the score
+        score = [sum(n)/len(n) for n in self.res]
+        test_texts, fpaths, score, durations = sort(self.selection_texts, self.selection_fpaths, score, self.selection_durations)
+        samples = []
+        selected_duration = 0.0
+
+        path_score = {}
+        for text, fpath, this_score in zip(test_texts, fpaths, score):
+            path_score[fpath] = this_score
+
+
+        while (selected_duration < required_duration and len(test_texts) > 0):
+            # first, start with the case with the highest score
+            text = test_texts.pop(0)
+            fpath = fpaths.pop(0)
+            this_score = score.pop(0)
+            duration = durations.pop(0)
+
+            selected_duration += duration
+
+            samples.append(json.dumps({"text": text, "audio_filepath": fpath, "duration": duration}) + "\n")
+
+            # after selecting a test case, we need to update the phoneme frequency
+            self.acc_phone_freqs += self.sent_wise_phone_freqs[fpath]['freq']
+
+            # update the score for the remaining test cases
+            new_scores = []
+            for i in range(len(score)):
+                new_path = fpaths[i]
+                f_old = get_f(self.acc_phone_freqs)
+                f_new = get_f(self.acc_phone_freqs + self.sent_wise_phone_freqs[new_path]['freq'])
+                new_score = (f_new - f_old) * path_score[new_path]
+                new_score = np.sum(new_score)
+                new_scores.append(new_score)
+            
+            test_texts, fpaths, score, durations = sort(test_texts, fpaths, new_scores, durations)
+
+        return samples
+
+
 
     def sample_with_word_enhancing(self, duration: float):
         pass
 
+    def get_phonemes(self, text):
+        '''compute the phoneme frequency of a text'''
+        phone_list = get_phoneme_seq(text)
+        phone_list = [item if len(item)!=3 else item[:-1] for item in phone_list]
+        phone_list = [item for item in phone_list if item not in {"'"}]
+        return phone_list
+
+    def get_sentence_wise_phone_freqs(self):
+        '''compute the phoneme frequency of each sentence'''
+        self.phone_vocab = phone_vocab
+        self.acc_phone_freqs = np.zeros(len(self.phone_vocab))
+        self.sent_wise_phone_freqs = {}
+        self.phone_to_id = dict([(phone,i) for i,phone in enumerate(self.phone_vocab)])
+        for text, path in zip(self.selection_texts, self.selection_fpaths):
+            freq = np.zeros(len(self.phone_vocab))
+            phones = self.get_phonemes(text)
+            for phone in phones:
+                freq[self.phone_to_id[phone]] +=1
+            self.sent_wise_phone_freqs[path] = {'freq': freq, 'phones': phones, 'text': text}
+
+        return self.sent_wise_phone_freqs
 
 
 def main(args):
@@ -172,6 +263,7 @@ def main(args):
     np.random.seed(args.seed)
     torch.manual_seed(args.seed)
 
+    # compute the required duration to be sampled
     seed_samples = [line for line in open(args.seed_json_file)]
     random_samples_duration = sum([json.loads(line.strip())['duration'] for line in open(args.random_json_file)])
     seed_duration = sum([json.loads(line.strip())['duration'] for line in open(args.seed_json_file)])
@@ -182,9 +274,12 @@ def main(args):
     err_sampler = WordErrorSampler(args.finetuned_ckpt, args.selection_json_file)
     err_sampler.load_model()
     err_sampler.load_sampling_data()
-    samples = err_sampler.normal_sample(required_duration)
+    err_sampler.get_sentence_wise_phone_freqs()
+    samples = err_sampler.sample_with_phone_enhancing(required_duration)
 
     # dump the samples
+
+    exit()
     output_json_file = os.path.join(args.output_json_path, str(
         args.num_sample), args.sampling, 'seed_' + str(args.seed), 'train.json')
 
