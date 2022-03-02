81ce1f61b88f6e8d88cf1cc81425b80567a53534
Muhammad Hilmi Asyrofi <mhilmiasyrofi@gmail.com>
2022-03-02 12:51:48 +0800

    chore(deepspeech_l2arctic): fine tune error model on random sample

--- models/deepspeech_asr/scripts/l2arctic/finetune_on_randomly_selected_samples.sh
+++ models/deepspeech_asr/scripts/l2arctic/finetune_on_randomly_selected_samples.sh
@@ -5,7 +5,7 @@ declare -a accents=('ASI')
 
 for seed in 1
 do 
-  for size in 1500
+  for size in 50
   do
     for accent in "${accents[@]}"
     do
@@ -15,11 +15,11 @@ do
       model_dir=$PRETRAINED_CKPTS/deepspeech/finetuned/$accent/$size/seed_"$seed"/random
       mkdir -p $model_dir
       python3 -u finetune.py \
-        --train_manifest=$DATA/$accent/manifests/train/deepspeech/random/$size/seed_"$seed"/train.json \
+        --train_manifest=$DATA/$accent/manifests/train/random/$size/seed_"$seed"/train.json \
         --val_manifest=$DATA/$accent/manifests/dev.json \
         --wav_dir=$WAV_DIR \
         --load_checkpoint_dir=$PRETRAINED_CKPTS/deepspeech/checkpoints/deepspeech-0.9.3-checkpoint/ \
-        --save_checkpoint_dir=$PRETRAINED_CKPTS/deepspeech/checkpoints/deepspeech-random-checkpoint/ \
+        --save_checkpoint_dir=$PRETRAINED_CKPTS/deepspeech/checkpoints/deepspeech-random-size_"$size"-seed_"$seed"-checkpoint/ \
         --model_scorer=$PRETRAINED_CKPTS/deepspeech/deepspeech-0.9.3-models.scorer \
         --output_dir=$model_dir/recent \
         --gpu_id=0 \
