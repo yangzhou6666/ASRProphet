54caa271b55eac1ebcda708f9534d0e783e12b25
Muhammad Hilmi Asyrofi <mhilmiasyrofi@gmail.com>
2022-03-09 10:41:25 +0800

    feat(analyze): generate data from a list

--- analyze.py
+++ analyze.py
@@ -30,15 +30,22 @@ def gather_result(asr:str, dataset:str, tool:str):
         dataset: dataset name
         tool: tool name
     """
-    
     data_path = f"data/l2arctic/processed/{dataset}/manifests/train/{asr}/{tool}/"
+    
     df = pd.DataFrame(columns=['Dataset', 'Seed', 'Size', 'WER', 'CER'])
     
     for seed in [1, 2, 3]:
         
         for size in [50, 75, 100, 150, 200, 300, 400, 500]:
             size = str(size)
-            path_to_log = os.path.join(data_path, size,f"seed_{seed}", "test_out_ori_log.txt")
+            
+            if tool == "error_model":
+                path_to_log = os.path.join(data_path, size,f"seed_{seed}", "test_out_ori_log.txt")
+            elif tool == "word_error_predictor_real" :
+                path_to_log = os.path.join(data_path, size, "word_enhance", f"seed_{seed}", "test_out_ori_log.txt")
+            else :
+                raise ValueError("Undefined tool")
+
             try:
                 WER, CER = analyze_result(path_to_log)
             except:
@@ -59,11 +66,22 @@ def gather_result(asr:str, dataset:str, tool:str):
 
 
 if __name__ == "__main__":
-    asr = "deepspeech"
-    dataset = "ASI"
-    tool = "error_model"
-    df = gather_result(asr, dataset, tool)
-    os.makedirs("result", exist_ok=True)
-    df.to_csv(f"result/{asr}_{dataset}_{tool}.csv")
 
-    print(df)
+    asrs = ["deepspeech"]
+    datasets = ["ASI", "RRBI"]
+    tools = ["error_model", "word_error_predictor_real"]
+    
+    for asr in asrs :
+        for dataset in datasets :
+            for tool in tools :
+        
+                print()
+                print("ASR \t\t: ", asr)
+                print("Dataset \t: ", dataset)
+                print("Tool \t\t: ", tool)
+
+                df = gather_result(asr, dataset, tool)
+                print(df)
+
+                os.makedirs("result", exist_ok=True)
+                df.to_csv(f"result/{asr}_{dataset}_{tool}.csv")
