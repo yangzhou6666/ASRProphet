822b983e23f35e10d1db292904ddfb76f582b8c8
jiekeshi <jiekeshi125@gmail.com>
2022-03-10 10:35:10 +0800

    feat: fine-tune wav2vec and hubert

--- models/wav2vec2_asr/finetune.py
+++ models/wav2vec2_asr/finetune.py
@@ -1,227 +1,148 @@
-# Source:  https://huggingface.co/blog/fine-tune-wav2vec2-english
-
-from transformers import Trainer
-from transformers import TrainingArguments
-from transformers import Wav2Vec2ForCTC
-from typing import Any, Dict, List, Optional, Union
-from dataclasses import dataclass, field
+import os
 import torch
-import soundfile as sf
-from transformers import Wav2Vec2FeatureExtractor
-from transformers import Wav2Vec2Processor
-from transformers import Wav2Vec2CTCTokenizer
-import json
-import re
-from datasets import load_dataset, load_metric
-import numpy as np
+import random
+import helpers
 import argparse
-
-
-@dataclass
-class DataCollatorCTCWithPadding:
-    """
-    Data collator that will dynamically pad the inputs received.
-    Args:
-        processor (:class:`~transformers.Wav2Vec2Processor`)
-            The processor used for proccessing the data.
-        padding (:obj:`bool`, :obj:`str` or :class:`~transformers.tokenization_utils_base.PaddingStrategy`, `optional`, defaults to :obj:`True`):
-            Select a strategy to pad the returned sequences (according to the model's padding side and padding index)
-            among:
-            * :obj:`True` or :obj:`'longest'`: Pad to the longest sequence in the batch (or no padding if only a single
-            sequence if provided).
-            * :obj:`'max_length'`: Pad to a maximum length specified with the argument :obj:`max_length` or to the
-            maximum acceptable input length for the model if that argument is not provided.
-            * :obj:`False` or :obj:`'do_not_pad'` (default): No padding (i.e., can output a batch with sequences of
-            different lengths).
-        max_length (:obj:`int`, `optional`):
-            Maximum length of the ``input_values`` of the returned list and optionally padding length (see above).
-        max_length_labels (:obj:`int`, `optional`):
-            Maximum length of the ``labels`` returned list and optionally padding length (see above).
-        pad_to_multiple_of (:obj:`int`, `optional`):
-            If set will pad the sequence to a multiple of the provided value.
-            This is especially useful to enable the use of Tensor Cores on NVIDIA hardware with compute capability >=
-            7.5 (Volta).
-    """
-
-    processor: Wav2Vec2Processor
-    padding: Union[bool, str] = True
-    max_length: Optional[int] = None
-    max_length_labels: Optional[int] = None
-    pad_to_multiple_of: Optional[int] = None
-    pad_to_multiple_of_labels: Optional[int] = None
-
-    def __call__(self, features: List[Dict[str, Union[List[int], torch.Tensor]]]) -> Dict[str, torch.Tensor]:
-        # split inputs and labels since they have to be of different lenghts and need
-        # different padding methods
-        input_features = [{"input_values": feature["input_values"]}
-                        for feature in features]
-        label_features = [{"input_ids": feature["labels"]}
-                        for feature in features]
-
-        batch = self.processor.pad(
-            input_features,
-            padding=self.padding,
-            max_length=self.max_length,
-            pad_to_multiple_of=self.pad_to_multiple_of,
-            return_tensors="pt",
-            )
-        with self.processor.as_target_processor():
-            labels_batch = self.processor.pad(
-                label_features,
-                padding=self.padding,
-                max_length=self.max_length_labels,
-                pad_to_multiple_of=self.pad_to_multiple_of_labels,
-                return_tensors="pt",
-            )
-
-        # replace padding with -100 to ignore loss correctly
-        labels = labels_batch["input_ids"].masked_fill(
-            labels_batch.attention_mask.ne(1), -100)
-
-        batch["labels"] = labels
-
-        return batch
-
-
-def remove_special_characters(batch):
-    batch["text"] = re.sub(chars_to_ignore_regex, '', batch["text"]).lower()
-    return batch
-
-
-def extract_all_chars(batch):
-    all_text = " ".join(batch["text"])
-    vocab = list(set(all_text))
-    return {"vocab": [vocab], "all_text": [all_text]}
-
-def prepare_dataset(batch):
-
-    audio_array, _ = sf.read(batch["audio_filepath"])
-
-    # batched output is "un-batched" to ensure mapping is correct
-    batch["input_values"] = processor(
-        audio_array, sampling_rate=16000).input_values[0]
-
-    with processor.as_target_processor():
-        batch["labels"] = processor(batch["text"]).input_ids
-    return batch
-
-def compute_metrics(pred):
-    pred_logits = pred.predictions
-    pred_ids = np.argmax(pred_logits, axis=-1)
-
-    pred.label_ids[pred.label_ids == -
-                    100] = processor.tokenizer.pad_token_id
-
-    pred_str = processor.batch_decode(pred_ids)
-    # we do not want to group tokens when computing the metrics
-    label_str = processor.batch_decode(pred.label_ids, group_tokens=False)
-
-    wer = wer_metric.compute(predictions=pred_str, references=label_str)
-
-    return {"wer": wer}
+import numpy as np
+from dataclasses import dataclass
+from datasets import load_dataset, Audio
+from typing import Dict, List, Optional, Union
+from transformers import AutoModelForCTC, Wav2Vec2Processor, Trainer, TrainingArguments, Wav2Vec2CTCTokenizer, Wav2Vec2FeatureExtractor
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='Fine-tuning Wav2Vec2')
-    parser.add_argument("--batch_size", default=32,
-                        type=int, help='data batch size')
-    parser.add_argument("--train_manifest", type=str, required=True,
-                        help='relative path given dataset folder of training manifest file')
-    parser.add_argument("--val_manifest", type=str, required=True,
-                        help='relative path given dataset folder of evaluation manifest file')
-    parser.add_argument("--output_dir", type=str, required=True,
-                        help='saves results in this directory')
-    parser.add_argument("--vocab", type=str, required=True,
-                        help='saves vocab.json for tokenizer')
+    parser = argparse.ArgumentParser(description='wav2vec2')
+
+    parser.add_argument("--wav_dir", type=str, help='directory to the wav file')
+    parser.add_argument("--train_manifest", type=str, required=True, help='relative path to train dataset manifest file')
+    parser.add_argument("--val_manifest", type=str, required=True, help='relative path to evaluation dataset manifest file')
+    parser.add_argument("--output_dir", default="./", type=str)
+    parser.add_argument("--model", default="wav2vec", type=str)
     parser.add_argument("--seed", default=42, type=int, help='seed')
-    parser.add_argument("--num_epochs", default=30, type=int,
-                        help='number of training epochs. if number of steps if specified will overwrite this')
-    parser.add_argument("--learning_rate", default=1e-4,
-                        type=float, help='learning rate')
+    return parser.parse_args()
 
-    args = parser.parse_args()
-    return args
 
+def main(args):
+    models = {"wav2vec": "facebook/wav2vec2-base-960h",
+              "hubert": "facebook/hubert-large-ls960-ft"}
+
+    feature_extractor = Wav2Vec2FeatureExtractor.from_pretrained(models[args.model])
+    tokenizer = Wav2Vec2CTCTokenizer.from_pretrained(models[args.model])
+    processor = Wav2Vec2Processor(feature_extractor=feature_extractor, tokenizer=tokenizer)
+
+    model = AutoModelForCTC.from_pretrained(models[args.model], ctc_loss_reduction="mean", pad_token_id=processor.tokenizer.pad_token_id)
+    
+
+    @dataclass
+    class DataCollatorCTCWithPadding:
+    
+        processor: Wav2Vec2Processor
+        padding: Union[bool, str] = True
+        max_length: Optional[int] = None
+        max_length_labels: Optional[int] = None
+        pad_to_multiple_of: Optional[int] = None
+        pad_to_multiple_of_labels: Optional[int] = None
+
+        def __call__(self, features: List[Dict[str, Union[List[int], torch.Tensor]]]) -> Dict[str, torch.Tensor]:
+            input_features = [{"input_values": feature["input_values"]} for feature in features]
+            label_features = [{"input_ids": feature["labels"]} for feature in features]
+
+            batch = self.processor.pad(
+                input_features,
+                padding=self.padding,
+                max_length=self.max_length,
+                pad_to_multiple_of=self.pad_to_multiple_of,
+                return_tensors="pt",
+            )
+            with self.processor.as_target_processor():
+                labels_batch = self.processor.pad(
+                    label_features,
+                    padding=self.padding,
+                    max_length=self.max_length_labels,
+                    pad_to_multiple_of=self.pad_to_multiple_of_labels,
+                    return_tensors="pt",
+                )
+            labels = labels_batch["input_ids"].masked_fill(labels_batch.attention_mask.ne(1), -100)
+            batch["labels"] = labels
+
+            return batch
+
+    data_collator = DataCollatorCTCWithPadding(processor=processor, padding=True)
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model.to(device)
+    model.freeze_feature_extractor()
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+
+    def add_prefix(example):
+        example['audio_filepath'] = os.path.join(args.wav_dir, example['audio_filepath'])
+        return example
+    
+    def prepare_dataset(batch):
+        audio = batch["audio_filepath"]
+        batch["input_values"] = processor(audio["array"], sampling_rate=audio["sampling_rate"]).input_values[0]
+        with processor.as_target_processor():
+            batch["labels"] = processor(batch["text"].upper()).input_ids
+        return batch
+
+    data_files = {"train":args.train_manifest, "validation": args.val_manifest}
+    train_dataset = load_dataset("json", data_files=data_files, split="train")
+    train_dataset = train_dataset.map(add_prefix)
+    train_dataset = train_dataset.cast_column("audio_filepath", Audio(sampling_rate=16_000))
+    train_dataset = train_dataset.map(prepare_dataset, num_proc=4)
+
+    eval_dataset = load_dataset("json", data_files=data_files, split="validation")
+    eval_dataset = eval_dataset.map(add_prefix)
+    eval_dataset = eval_dataset.cast_column("audio_filepath", Audio(sampling_rate=16_000))
+    eval_dataset = eval_dataset.map(prepare_dataset, num_proc=4)
+
+    def compute_metrics(pred):
+        pred_logits = pred.predictions
+        pred_ids = np.argmax(pred_logits, axis=-1)
+
+        pred.label_ids[pred.label_ids == -100] = processor.tokenizer.pad_token_id
+
+        pred_str = processor.batch_decode(pred_ids)
+        label_str = processor.batch_decode(pred.label_ids, group_tokens=False)
+
+        WER, _, _, _ = helpers.word_error_rate(hypotheses=pred_str, references=label_str)
+
+        return {"wer": WER*100}
+
+    training_args = TrainingArguments(
+        output_dir="./test",
+        group_by_length=True,
+        per_device_train_batch_size=32,
+        evaluation_strategy="epoch",
+        save_strategy = "epoch",
+        num_train_epochs=30,
+        gradient_checkpointing=True, 
+        save_steps=500,
+        eval_steps=500,
+        logging_steps=500,
+        learning_rate=1e-4,
+        weight_decay=0.005,
+        warmup_steps=1000,
+        save_total_limit=2,
+        load_best_model_at_end=True
+        )
+    
+    trainer = Trainer(
+            model=model,
+            data_collator=data_collator,
+            args=training_args,
+            compute_metrics=compute_metrics,
+            train_dataset=train_dataset,
+            eval_dataset=eval_dataset,
+            tokenizer=processor.feature_extractor
+        )
+    
+    trainer.train()
+    trainer.save_model(args.output_dir)
 
 if __name__ == "__main__":
-
     args = parse_args()
-
-    data_files = {"train": args.train_manifest,
-                "test": args.val_manifest}
-    
-    data = load_dataset("json", data_files=data_files)
-
-    data.remove_columns("duration")
-    chars_to_ignore_regex = '[\,\?\.\!\-\;\:\"]'
-
-    data = data.map(remove_special_characters)
-
-
-    vocabs = data.map(extract_all_chars, batched=True, batch_size=-1,
-                    keep_in_memory=True, remove_columns=data.column_names["train"])
-
-    vocab_list = list(set(vocabs["train"]["vocab"][0])
-                    | set(vocabs["test"]["vocab"][0]))
-
-    vocab_dict = {v: k for k, v in enumerate(vocab_list)}
-    vocab_dict["|"] = vocab_dict[" "]
-    del vocab_dict[" "]
-    vocab_dict["[UNK]"] = len(vocab_dict)
-    vocab_dict["[PAD]"] = len(vocab_dict)
-    
-    with open(args.vocab, 'w') as vocab_file:
-        json.dump(vocab_dict, vocab_file)
-
-    # tokenizer = Wav2Vec2CTCTokenizer(
-    #     args.vocab, unk_token="[UNK]", pad_token="[PAD]", word_delimiter_token="|")
-
-    # feature_extractor = Wav2Vec2FeatureExtractor(
-    #     feature_size=1, sampling_rate=16000, padding_value=0.0, do_normalize=True, return_attention_mask=False)
-
-    # processor = Wav2Vec2Processor(
-    #     feature_extractor=feature_extractor, tokenizer=tokenizer)
-
-    # data_prepared = data.map(
-    #     prepare_dataset, remove_columns=data.column_names["train"], num_proc=4)
-    
-    # data_collator = DataCollatorCTCWithPadding(processor=processor, padding=True)
-    # wer_metric = load_metric("wer")
-
-    # model = Wav2Vec2ForCTC.from_pretrained(
-    #     "facebook/wav2vec2-base",
-    #     ctc_loss_reduction="mean",
-    #     pad_token_id=processor.tokenizer.pad_token_id,
-    # )
-    
-    # model.freeze_feature_extractor()
-
-    # training_args = TrainingArguments(
-    #     output_dir=args.output_dir,
-    #     group_by_length=True,
-    #     per_device_train_batch_size=args.batch_size,
-    #     evaluation_strategy="steps",
-    #     num_train_epochs=args.num_epochs,
-    #     fp16=True,
-    #     gradient_checkpointing=True,
-    #     save_steps=500,
-    #     eval_steps=500,
-    #     logging_steps=500,
-    #     learning_rate=args.learning_rate,
-    #     weight_decay=0.005,
-    #     warmup_steps=1000,
-    #     save_total_limit=2,
-    # )
-
-    # trainer = Trainer(
-    #     model=model,
-    #     data_collator=data_collator,
-    #     args=training_args,
-    #     compute_metrics=compute_metrics,
-    #     train_dataset=data_prepared["train"],
-    #     eval_dataset=data_prepared["test"],
-    #     tokenizer=processor.feature_extractor,
-    # )
-    
-    # trainer.train()
-
+    helpers.print_dict(vars(args))
+    main(args)
