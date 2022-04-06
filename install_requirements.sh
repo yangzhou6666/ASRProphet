4a2d84fbc8a35a989626f21db267fd4941ae4367
jiekeshi <jiekeshi125@gmail.com>
2022-04-06 16:11:58 +0800

    chore: install Nvidia apex

--- install_requirements.sh
+++ install_requirements.sh
@@ -2,4 +2,15 @@ pip install -r requirements.txt
 python -c "import nltk; nltk.download('brown')"
 python -c "import nltk; nltk.download('names')"
 apt update
-apt isntall sox
+apt install sox -y
+apt install libsndfile1 -y
+
+cd ../
+apt update
+apt install git -y
+git clone https://github.com/NVIDIA/apex
+cd apex
+pip install -v --disable-pip-version-check --no-cache-dir ./
+
+pip install torch==1.5.1+cu101 torchvision==0.6.1+cu101 -f https://download.pytorch.org/whl/torch_stable.html
+
