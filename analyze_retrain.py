ce5105521ebfbc4d292c1b2ab998caca622531b5
jiekeshi <jiekeshi125@gmail.com>
2022-04-06 23:32:57 +0800

    feat(analyze): combine results from various dataset

--- analyze_retrain.py
+++ analyze_retrain.py
@@ -8,6 +8,18 @@ from typing import List
 warnings.filterwarnings("ignore")
 
 
+shorten_col_name = {
+    "WER_Seed1": "W1", 
+    "WER_Seed2": "W2",
+    "WER_Seed3": "W3",
+    "WER_Avg": "Wa",
+    "CER_Seed1": "C1",
+    "CER_Seed2": "C2",
+    "CER_Seed3": "C3",
+    "CER_Avg": "Ca"
+}
+
+
 def analyze_result(log_path):
     if os.path.exists(log_path): 
         with open(log_path) as f:
@@ -18,7 +30,7 @@ def analyze_result(log_path):
             CER = contents[-2].strip()
             CER = CER.split(" ")[-1]
     else:
-        # print("\033[0;37;41m\t!!Data Missing!!\033[0m")
+        print("\033[0;37;41m\t!!Data Missing!!\033[0m")
         WER = -1
         CER = -1
     assert len(WER) < 6
@@ -51,7 +63,7 @@ def gather_result(asr:str, dataset:str, tool:str):
 
             
             
-            if tool in ["icassp_real_mix", "icassp_real_mix_lr1e-5"]:
+            if tool in ["icassp_real_mix", "asrevolve_error_model_real", "icassp_real_mix_lr1e-5"]:
                 path_to_log = f"models/pretrained_checkpoints/{asr}/finetuned/{dataset}/{size}/seed_{seed}/{tool}/test_infer_log.txt"
             elif tool in ["word_error_real_mix", "word_error_real_mix_lr1e-5"] :
                 path_to_log = f"models/pretrained_checkpoints/{asr}/finetuned/{dataset}/{size}/seed_{seed}/{tool}/word_enhance/test_infer_log.txt"
@@ -65,7 +77,7 @@ def gather_result(asr:str, dataset:str, tool:str):
             try:
                 WER, CER = analyze_result(path_to_log)
             except:
-                # print(path_to_log)
+                print(path_to_log)
                 WER = -1
                 CER = -1
             
@@ -112,9 +124,42 @@ def combine_result(wer:str, cer:str, datas: List[pd.DataFrame])-> pd.DataFrame:
 
     for i, df in enumerate(datas):
         for col in ['WER_Seed1', 'WER_Seed2', 'WER_Seed3', 'WER_Avg', 'CER_Seed1', 'CER_Seed2', 'CER_Seed3', 'CER_Avg'] :
-            combined_df[f"{col}_t{i}"] = df[col]
+            combined_df[f"{shorten_col_name[col]}_t{i}"] = df[col]
 
     return combined_df
+
+
+def combine_tools(datas: List[pd.DataFrame])-> pd.DataFrame:
+    """combine results from various tools into one dataframe
+    :param datas: result from various tool
+    :return: horisontally combined dataframe
+    """
+    data = {"Size": datas[0]["Size"]}
+
+    combined_df = pd.DataFrame(data)
+
+    for i, df in enumerate(datas):
+        for col in ['WER_Seed1', 'WER_Seed2', 'WER_Seed3', 'WER_Avg', 'CER_Seed1', 'CER_Seed2', 'CER_Seed3', 'CER_Avg'] :
+            combined_df[f"{shorten_col_name[col]}_t{i}"] = df[col]
+
+    return combined_df
+
+def combine_dataset(dataset_names, datas: List[pd.DataFrame])-> pd.DataFrame:
+    """combine results from various datasets into one dataframe
+    :param datas: result from various dataset
+    :return: vertically combined dataframe
+    """
+    
+    for i in range(len(datas)) :
+        datas[i]["Name"] = dataset_names[i]
+    
+        cols = datas[i].columns.to_list()
+        cols = cols[-1:] + cols[:-1]
+        datas[i] = datas[i][cols]
+        
+    result = pd.concat(datas)
+
+    return result
 
 
 def get_original_performance(asr:str, dataset:str):
@@ -144,23 +189,15 @@ if __name__ == "__main__":
     # datasets = ["SVBI", "HJK"]
     # tools = [ "icassp_real_mix", "word_error_real_mix"]
     
-    # asrs = ["hubert"]
-    # datasets = ["ABA", "SKA", "YBAA", "ZHAA", "BWC", "LXC", "NCC", "TXHC", "HJK", "HKK", "YDCK", "YKWK", "ASI", "RRBI", "SVBI", "TNI", "EBVS", "ERMS", "MBMPS", "NJS", "HQTV", "PNV", "THV", "TLV"]
-    # datasets = ["BWC", "LXC"]
-    # tools = [ "icassp_real_mix", "word_error_real_mix"]
-    
-
-    asrs = ["wav2vec"]
+    asrs = ["hubert"]
     datasets = ["YBAA", "ZHAA", "ASI", "TNI", "NCC", "TXHC", "EBVS", "ERMS", "YDCK", "YKWK", "THV", "TLV"]
-    tools = [ "icassp_real_mix_lr1e-5", "word_error_real_mix_lr1e-5"]
-
-    # asrs = ["wavlm"]
-    # datasets = ["YBAA", "ZHAA", "ASI", "TNI", "NCC", "TXHC", "EBVS", "ERMS", "YDCK", "YKWK", "THV", "TLV"]
-    # tools = [ "icassp_real_mix", "word_error_real_mix"]
-    
+    tools = [ "asrevolve_error_model_real"]
     
     
     for asr in asrs :
+        
+        dataframes = []
+        
         for dataset in datasets :
             wer, cer = get_original_performance(asr, dataset)
 
@@ -183,9 +220,17 @@ if __name__ == "__main__":
                 dfs.append(df)
             
             combined_df = combine_result(wer, cer, dfs)
+            # combined_df = combine_tools(dfs)
             combined_df.drop(columns=["Size"], inplace=True)
             
-            print()
-            print("ASR \t\t: ", asr)
-            print("Dataset \t: ", dataset)
-            print(combined_df.to_string(index=False))
+            # print()
+            # print("ASR \t\t: ", asr)
+            # print("Dataset \t: ", dataset)
+            # print(combined_df.to_string(index=False))
+            
+            dataframes.append(combined_df)
+            
+        # print()
+        # print()
+        # result = combine_dataset(datasets, dataframes)
+        # print(result.to_string(index=False))
