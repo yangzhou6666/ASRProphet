f2095ada326351424d4e8deb0831ed9747e7ccf6
Muhammad Hilmi Asyrofi <mhilmiasyrofi@gmail.com>
2022-04-11 20:55:05 +0800

    feat(sampling): initialize the diversity driven sampling

---
+++ models/error_model/diversity_sampling.py
@@ -0,0 +1,11 @@
+
+
+class DiversityDrivenSampler():
+    '''
+    The class for two diversity-drive test case selection methods:
+    (1) A Method for the Extraction of Phonetically-Rich Triphone Sentences
+    (2) A submodular optimization approach to sentence set selection
+    '''
+
+    def __init__(self):
+        pass
