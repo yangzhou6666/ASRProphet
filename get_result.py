300a2c503f1c752b5e4789cbc46ae61261f001be
Muhammad Hilmi Asyrofi <mhilmiasyrofi@gmail.com>
2022-04-21 07:57:59 +0800

    refactor(analyze-RQ3): wrap the code into several modular functions

--- get_result.py
+++ get_result.py
@@ -6,13 +6,13 @@ import os
 import helper
 import json
 
+asrs = ["quartznet", "hubert", "wav2vec-base"]
+datasets = ["YBAA", "ZHAA", "ASI", "TNI", "NCC", "TXHC", "EBVS", "ERMS", "YDCK", "YKWK", "THV", "TLV"]
+    
 
-if __name__ == "__main__":
-    
-    asrs = ["quartznet", "hubert", "wav2vec-base"]
-    datasets = ["YBAA", "ZHAA", "ASI", "TNI", "NCC", "TXHC", "EBVS", "ERMS", "YDCK", "YKWK", "THV", "TLV"]
-    
-    ## RQ1: Measure the WER and CER using the original model
+def save_performance_of_original_model_on_test_set():
+    """Measure the WER and CER using the original model"""
+
     tools = ["random", "error_model_triphone_rich","error_model_pure_diversity", "error_model_without_diversity_enhancing", "error_model", "asrevolve_error_model_real", "word_error_predictor_real/no_word_enhance", "word_error_predictor_real/word_enhance"]
 
     os.makedirs("result/RQ1", exist_ok=True)
@@ -30,8 +30,11 @@ if __name__ == "__main__":
     
     with open('result/RQ1.json', 'w') as fp:
         json.dump(raw_results, fp)    
+
+
+def save_performance_of_finetuned_model_on_test_set():
+    """Measure the WER and CER using the fine-tuned model"""
     
-    ## RQ2: Measure the WER and CER using the fine-tuned model            
     tools = ["random", "triphone_rich", "pure_diversity", "icassp_without_diversity_enhancing_real_mix", "icassp_real_mix", "asrevolve_error_model_real", "word_error_real_mix/no_word_enhance", "word_error_real_mix/word_enhance"]
     
     os.makedirs("result/RQ2", exist_ok=True)
@@ -51,7 +54,8 @@ if __name__ == "__main__":
         json.dump(raw_results, fp)    
     
 
-    ## Measure the WER and CER using the original model            
+def save_performance_of_original_model_on_the_dataset():
+    """Measure the WER and CER using the original model"""
     
     raw_results = {}
     for asr in asrs :
@@ -67,5 +71,13 @@ if __name__ == "__main__":
     
     with open('result/original.json', 'w') as fp:
         json.dump(raw_results, fp)  
+    
+    
+
+if __name__ == "__main__":
+    
+    save_performance_of_original_model_on_test_set()
+    save_performance_of_original_model_on_the_dataset()
+    save_performance_of_original_model_on_the_dataset()
       
     
