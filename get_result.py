0309b128ad4d7da928f45bc523fde37f85d263a7
Muhammad Hilmi Asyrofi <mhilmiasyrofi@gmail.com>
2022-04-21 11:56:03 +0800

    feat(analyze-RQ3): measure the phone submodular function

--- get_result.py
+++ get_result.py
@@ -82,24 +82,76 @@ def save_performance_of_original_model_on_the_dataset():
 def save_the_distance_between_the_triphone_rich_selected_samples_and_ideal_distributions():
     """Measure the distance between the triphone-rich selected samples and ideal distributions"""
 
+    tools = ["random", "error_model_triphone_rich","error_model_pure_diversity", "error_model_without_diversity_enhancing", "error_model", "asrevolve_error_model_real", "word_error_predictor_real/no_word_enhance", "word_error_predictor_real/word_enhance"]
+    
     raw_results = {}
     for asr in asrs :
         raw_results[asr] = {}
         for dataset in datasets :
             raw_results[asr][dataset] = {}
-            for seed in [1, 2, 3] :
-                raw_results[asr][dataset][seed] = []        
-                for size in [100, 200, 300, 400] :
-                    fpath = f"{DATA}/{dataset}/manifests/train/{asr}/error_model_triphone_rich/{size}/seed_{seed}/train.json"
-                    sampler = ErrorModelSampler(fpath, "triphone_rich", error_model_weights=None, verbose=False)
-                    assert len(sampler.phone_freqs) == len(sampler.ideal_triphone_dists)
-                    triphone_dists = sampler.phone_freqs / sampler.phone_freqs.sum()
-                    assert 0.95 < triphone_dists.sum() and triphone_dists.sum() < 1.05
-                    assert 0.95 < sampler.ideal_triphone_dists.sum() and sampler.ideal_triphone_dists.sum() < 1.05
-                    distance = sampler.compute_euclidean_distance(triphone_dists, sampler.ideal_triphone_dists)
-                    raw_results[asr][dataset][seed].append(distance)
+            for tool in tools :
+                raw_results[asr][dataset][tool] = {}
+                for seed in [1, 2, 3] :
+                    raw_results[asr][dataset][tool][seed] = []        
+                    for size in [100, 200, 300, 400] :
+                        if tool == "random":
+                            fpath = f"{DATA}/{dataset}/manifests/train/{tool}/{size}/seed_{seed}/train.json"
+                        elif tool in ["word_error_predictor_real/no_word_enhance", "word_error_predictor_real/word_enhance"] :
+                            fpath = f"{DATA}/{dataset}/manifests/train/{asr}/word_error_predictor_real/{size}/{tool.split('/')[-1]}/seed_{seed}/train.json"
+                        else :
+                            fpath = f"{DATA}/{dataset}/manifests/train/{asr}/{tool}/{size}/seed_{seed}/train.json"
+                        
+                        if os.path.exists(fpath):
+                            sampler = ErrorModelSampler(fpath, "triphone_rich", error_model_weights=None, verbose=False)
+                            assert len(sampler.phone_freqs) == len(sampler.ideal_triphone_dists)
+                            triphone_dists = sampler.phone_freqs / sampler.phone_freqs.sum()
+                            assert 0.95 < triphone_dists.sum() and triphone_dists.sum() < 1.05
+                            assert 0.95 < sampler.ideal_triphone_dists.sum() and sampler.ideal_triphone_dists.sum() < 1.05
+                            distance = sampler.compute_euclidean_distance(triphone_dists, sampler.ideal_triphone_dists)
+                        else :
+                            print(f"Missing: {fpath}")
+                            distance = -999
+                            
+                        raw_results[asr][dataset][tool][seed].append(distance)
                     
     with open('result/triphone_rich.json', 'w') as fp:
+        json.dump(raw_results, fp)
+        
+def save_the_value_of_phoneme_submodular_function_from_selected_samples():
+    """Measure the value of phoneme submodular function from selected samples"""
+
+    tools = ["random", "error_model_triphone_rich","error_model_pure_diversity", "error_model_without_diversity_enhancing", "error_model", "asrevolve_error_model_real", "word_error_predictor_real/no_word_enhance", "word_error_predictor_real/word_enhance"]
+    
+    raw_results = {}
+    for asr in asrs :
+        raw_results[asr] = {}
+        for dataset in datasets :
+            raw_results[asr][dataset] = {}
+            for tool in tools :
+                raw_results[asr][dataset][tool] = {}
+                for seed in [1, 2, 3] :
+                    raw_results[asr][dataset][tool][seed] = []        
+                    for size in [100, 200, 300, 400] :
+                        if tool == "random":
+                            fpath = f"{DATA}/{dataset}/manifests/train/{tool}/{size}/seed_{seed}/train.json"
+                        elif tool in ["word_error_predictor_real/no_word_enhance", "word_error_predictor_real/word_enhance"] :
+                            fpath = f"{DATA}/{dataset}/manifests/train/{asr}/word_error_predictor_real/{size}/{tool.split('/')[-1]}/seed_{seed}/train.json"
+                        else :
+                            fpath = f"{DATA}/{dataset}/manifests/train/{asr}/{tool}/{size}/seed_{seed}/train.json"
+                        if os.path.exists(fpath):
+                            sampler = ErrorModelSampler(fpath, "pure_diversity", error_model_weights=None, verbose=False)
+                            log_frequency = sampler.get_f2(sampler.phone_freqs)
+                            assert len(sampler.ideal_phone_dists) == len(log_frequency) 
+                            phone_submodular_value = np.dot(sampler.ideal_phone_dists, log_frequency )
+                            assert phone_submodular_value > 0
+                            assert isinstance(phone_submodular_value, float)
+                        else :
+                            print(f"Missing: {fpath}")
+                            phone_submodular_value = -999
+                        
+                        raw_results[asr][dataset][tool][seed].append(phone_submodular_value)
+                        
+    with open('result/phoneme_submodular_function.json', 'w') as fp:
         json.dump(raw_results, fp)
 
 if __name__ == "__main__":
@@ -107,7 +159,8 @@ if __name__ == "__main__":
     # save_performance_of_original_model_on_test_set()
     # save_performance_of_finetuned_model_on_test_set()
     # save_performance_of_original_model_on_the_dataset()
-    save_the_distance_between_the_triphone_rich_selected_samples_and_ideal_distributions()
+    # save_the_distance_between_the_triphone_rich_selected_samples_and_ideal_distributions()
+    save_the_value_of_phoneme_submodular_function_from_selected_samples()
     
     
     
